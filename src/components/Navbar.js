48edafb2f4af5144d4cab4eae7a9419ac7b80baa
shivacheripally <shivacheripally14@gmail.com>
2023-09-02 08:17:14 +0530

    Navbar Implementation is Successful!

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,88 +1,86 @@
-import React, { useEffect, useState } from "react";
-import styled from '../App.module.css';
-import Widget from './Widget';
+import React from "react";
+import './Navbar.css';
 
 export default function Navbar(){
-  const[fav, setFav] = useState([]);
+    return (
+        <>
+        <nav className="navbar navbar-expand-lg bg-black">
+          <div className="container-fluid">
+            <img src="https://i.ibb.co/7jwyxj0/logo.jpg" alt="logo" className="logo-img" />
+            <a className="navbar-brand company-name" href="#footer"><span className="white-color">Sri Mahathishwari Ikkath Sarees</span></a>
+            <div className="div">
+              <a className="navbar-brand " href="https://wa.me/919030903058" target="_blank" rel="noreferrer">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="white" className="bi bi-whatsapp" viewBox="0 0 16 16">
+                    <path d="M13.601 2.326A7.854 7.854 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.933 7.933 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.898 7.898 0 0 0 13.6 2.326zM7.994 14.521a6.573 6.573 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.557 6.557 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592zm3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.729.729 0 0 0-.529.247c-.182.198-.691.677-.691 1.654 0 .977.71 1.916.81 2.049.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232z"/>
+                  </svg>
+              </a>
 
-  const onClickFav = ()=>{
-    const fav = localStorage.getItem('fav');
-    console.log('fav',typeof fav);
-    setFav(fav);
-  }
-
-    return(
-<nav className="navbar navbar-expand-lg bg-body-tertiary">
-  <div className="container-fluid">
-    <img src="https://i.ibb.co/7jwyxj0/logo.jpg" alt="logo" className={styled.logo_img}/>
-    <a className="navbar-brand" href="#">Sri Mahathishwari Ikkath Sarees</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarNavDropdown">
-      <ul className="navbar-nav">
-        <li className="nav-item">
-          <a className="nav-link active" target="_blank" aria-current="page" href="https://wa.me/919030903058"><i className="bi bi-whatsapp"></i></a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" target="_blank" href="https://www.facebook.com/rspochampallyikkatsarees?mibextid=LQQJ4d"><i className="bi bi-facebook"></i></a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" target="_blank" href="https://instagram.com/pochampallyikkatsarees?igshid=MGU3ZTQzNzY="><i className="bi bi-instagram"></i></a>
-        </li>
-        <li className="nav-item">
-          <a data-bs-toggle="modal" data-bs-target="#staticBackdrop" className="nav-link" href="#"><i onClick={onClickFav} className="bi bi-bag-heart"></i></a>
-          <div className="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabIndex={-1} aria-labelledby="staticBackdropLabel" aria-hidden="true">
-          <div className="modal-dialog">
-          <div className="modal-content">
-          <div className="modal-header">
-          <h1 className="modal-title fs-5" id="staticBackdropLabel">Favorites</h1>
-          <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-        <div className="modal-body">  
-        {/* {fav && fav.map((item, index)=>{
-          console.log('Item', item);
-          return(
-            <Widget key={index} imageUrl={item.imageUrl} cost={item.cost} name={item.name}/>
-          );
-        })} */}
-        </div>
-      <div className="modal-footer">
-        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" className="btn btn-primay">Buy All</button>
-      </div>
-    </div>
-  </div>
-</div>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" target="_blank" href="https://youtube.com/@Mahathishwarisarees"><i className="bi bi-youtube"></i></a>
-        </li>
-        <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown link
-          </a>
-          <ul className="dropdown-menu">
-            <li><a className="dropdown-item" href="#"><span>Less than 7000</span></a></li>
-            <li><a className="dropdown-item" href="#"><span>7000-8000</span></a></li>
-            <li><a className="dropdown-item" href="#"><span>8000-9000</span></a></li>
-            <li><a className="dropdown-item" href="#"><span>9000-10000</span></a></li>
-            <li><a className="dropdown-item" href="#"><span>10000-11000</span></a></li>
-            <li><a className="dropdown-item" href="#"><span>More than 11000</span></a></li>            
-          </ul>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" target="_blank" href="mailto:shivacheripally14@gmail.com"><i className="bi bi-envelope-at"></i></a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" target="_blank" href="https://maps.app.goo.gl/fdmSG5Xr7YtsaHyUA?g_st=iw"><i className="bi bi-geo-alt-fill"></i></a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="tel: 9848963748"><i className="bi bi-telephone-outbound-fill"></i></a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
-    );
+              <a className="navbar-brand " href="https://www.facebook.com/rspochampallyikkatsarees?mibextid=LQQJ4d" target="_blank" rel="noreferrer">
+                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="blue" className="bi bi-facebook" viewBox="0 0 16 16">
+                  <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z"/>
+                </svg>
+              </a>
+  
+              <a className="navbar-brand " href="https://instagram.com/pochampallyikkatsarees?igshid=MGU3ZTQzNzY=" target="_blank" rel="noreferrer">
+                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="red" className="bi bi-instagram" viewBox="0 0 16 16">
+                  <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.917 3.917 0 0 0-1.417.923A3.927 3.927 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.916 3.916 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.926 3.926 0 0 0-.923-1.417A3.911 3.911 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0h.003zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599.28.28.453.546.598.92.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.47 2.47 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.478 2.478 0 0 1-.92-.598 2.48 2.48 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233 0-2.136.008-2.388.046-3.231.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92.28-.28.546-.453.92-.598.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045v.002zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92zm-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217zm0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334z"/>
+                </svg>
+              </a>
+                <svg type="button" data-bs-toggle="modal" data-bs-target="#staticBackdrop" xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="white" id="cartIcon1" className="bi bi-bag-heart" viewBox="0 0 16 16">
+                  <path id="cartIcon2" fill-rule="evenodd" d="M10.5 3.5a2.5 2.5 0 0 0-5 0V4h5v-.5Zm1 0V4H15v10a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V4h3.5v-.5a3.5 3.5 0 1 1 7 0ZM14 14V5H2v9a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1ZM8 7.993c1.664-1.711 5.825 1.283 0 5.132-5.825-3.85-1.664-6.843 0-5.132Z"/>
+                </svg>
+              <span className="nav-item dropdown white-color">
+                  <span className="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">Price</span>
+                <ul className="dropdown-menu">
+                  <li className="dropdown-item"><span>Less than 7000</span></li>
+                  <li className="dropdown-item"><span>7000-8000</span></li>
+                  <li className="dropdown-item"><span>8000-9000</span></li>
+                  <li className="dropdown-item"><span>9000-10000</span></li>
+                  <li className="dropdown-item"><span>10000-11000</span></li>
+                  <li className="dropdown-item"><span>More than 11000</span></li>
+                </ul>
+              </span>
+            </div>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="white" className="bi bi-list" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+              </svg>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNavDropdown">
+              <ul className="navbar-nav">
+                <li className="nav-item">
+                  <a className="nav-link dropdown-toggle" href="https://youtube.com/@Mahathishwarisarees" target="_blank" rel="noreferrer">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="red" className="bi bi-youtube" viewBox="0 0 16 16">
+                      <path d="M8.051 1.999h.089c.822.003 4.987.033 6.11.335a2.01 2.01 0 0 1 1.415 1.42c.101.38.172.883.22 1.402l.01.104.022.26.008.104c.065.914.073 1.77.074 1.957v.075c-.001.194-.01 1.108-.082 2.06l-.008.105-.009.104c-.05.572-.124 1.14-.235 1.558a2.007 2.007 0 0 1-1.415 1.42c-1.16.312-5.569.334-6.18.335h-.142c-.309 0-1.587-.006-2.927-.052l-.17-.006-.087-.004-.171-.007-.171-.007c-1.11-.049-2.167-.128-2.654-.26a2.007 2.007 0 0 1-1.415-1.419c-.111-.417-.185-.986-.235-1.558L.09 9.82l-.008-.104A31.4 31.4 0 0 1 0 7.68v-.123c.002-.215.01-.958.064-1.778l.007-.103.003-.052.008-.104.022-.26.01-.104c.048-.519.119-1.023.22-1.402a2.007 2.007 0 0 1 1.415-1.42c.487-.13 1.544-.21 2.654-.26l.17-.007.172-.006.086-.003.171-.007A99.788 99.788 0 0 1 7.858 2h.193zM6.4 5.209v4.818l4.157-2.408L6.4 5.209z"/>
+                    </svg>
+                </a>
+                </li>
+                <li className="nav-item">
+                  <a href="mailto:shivacheripally14@gmail.com" className="nav-link dropdown-toggle" target="_blank" rel="noreferrer">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="white" className="bi bi-envelope-at-fill" viewBox="0 0 16 16">
+                        <path d="M2 2A2 2 0 0 0 .05 3.555L8 8.414l7.95-4.859A2 2 0 0 0 14 2H2Zm-2 9.8V4.698l5.803 3.546L0 11.801Zm6.761-2.97-6.57 4.026A2 2 0 0 0 2 14h6.256A4.493 4.493 0 0 1 8 12.5a4.49 4.49 0 0 1 1.606-3.446l-.367-.225L8 9.586l-1.239-.757ZM16 9.671V4.697l-5.803 3.546.338.208A4.482 4.482 0 0 1 12.5 8c1.414 0 2.675.652 3.5 1.671Z"/>
+                        <path d="M15.834 12.244c0 1.168-.577 2.025-1.587 2.025-.503 0-1.002-.228-1.12-.648h-.043c-.118.416-.543.643-1.015.643-.77 0-1.259-.542-1.259-1.434v-.529c0-.844.481-1.4 1.26-1.4.585 0 .87.333.953.63h.03v-.568h.905v2.19c0 .272.18.42.411.42.315 0 .639-.415.639-1.39v-.118c0-1.277-.95-2.326-2.484-2.326h-.04c-1.582 0-2.64 1.067-2.64 2.724v.157c0 1.867 1.237 2.654 2.57 2.654h.045c.507 0 .935-.07 1.18-.18v.731c-.219.1-.643.175-1.237.175h-.044C10.438 16 9 14.82 9 12.646v-.214C9 10.36 10.421 9 12.485 9h.035c2.12 0 3.314 1.43 3.314 3.034v.21Zm-4.04.21v.227c0 .586.227.8.581.8.31 0 .564-.17.564-.743v-.367c0-.516-.275-.708-.572-.708-.346 0-.573.245-.573.791Z"/>
+                    </svg>
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a href="https://maps.app.goo.gl/fdmSG5Xr7YtsaHyUA?g_st=iw" className="nav-link dropdown-toggle" target="_blank" rel="noreferrer">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="yellow" className="bi bi-geo-alt-fill" viewBox="0 0 16 16">
+                      <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/>
+                    </svg>
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a href="tel:9030903058" className="nav-link dropdown-toggle">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="skyblue" className="bi bi-telephone-outbound-fill" viewBox="0 0 16 16">
+                      <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511zM11 .5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-1 0V1.707l-4.146 4.147a.5.5 0 0 1-.708-.708L14.293 1H11.5a.5.5 0 0 1-.5-.5z"/>
+                    </svg>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+        </>
+    )
 }
--- src/components/Webpage.js
+++ src/components/Webpage.js
@@ -1,7 +1,8 @@
 import React from "react";
-import Navbar from "./Navbar";
+// import Navbar from "./Navbar";
 import Body from "./Body";
 import Footer from './Footer';
+import Navbar from "./Navbar";
 
 export default function Webpage(){
     return(
